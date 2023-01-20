bc81869bba8e64072435cb4fc2e18b6d0be80a4d
Necryl <necrylthewayfarer@gmail.com>
2023-01-20 09:29:28 +0530

    build the search function and all the supporting functions for it

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 // eslint-disable-next-line no-unused-vars
 import mainStyles from "./../styles/main.css";
 
@@ -53,19 +52,100 @@ function findPossibleMoves(id, board) {
   ["uur", "rru", "rrd", "ddr", "ddl", "lld", "llu"].forEach((sequence) => {
     const newLoc = move(id, sequence);
     if (typeof board[newLoc] !== "undefined") {
-      moves.push(newLoc);
+      moves.push(board[newLoc]);
     }
   });
 
   return moves;
 }
 
-function search() {}
+function search(baseNode, targetNode) {
+  const queue = {
+    base: [baseNode],
+    target: [targetNode],
+  };
 
-function constructPath() {}
+  let searchStatus = true;
 
-function knightMoves() {}
+  const visitor = (node, steps) => {
+    return {
+      node,
+      steps,
+    };
+  };
+
+  Object.keys(queue).forEach((type) => {
+    queue[type][0].visitors[type].push(visitor(null, 0));
+  });
+
+  const process = (type, q) => {
+    let result = false;
+    const current = q.shift();
+    const parentRecord = current.visitors[type].reduce((final, record) => {
+      if (final.steps > record.steps) {
+        final = record;
+      }
+      return final;
+    });
+    const children = [];
+    current.moves.forEach((child) => {
+      if (child !== parentRecord.node) {
+        children.push(child);
+      }
+    });
+    const newRecord = visitor(current, parentRecord.steps + 1);
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+      child.visitors[type].push(newRecord);
+      if (child.visitors[type === "base" ? "target" : "base"].length !== 0) {
+        result = child;
+        break;
+      }
+      q.push(child);
+    }
+    return result;
+  };
+
+  let contactNode;
+  while (searchStatus) {
+    // eslint-disable-next-line no-loop-func
+    Object.keys(queue).forEach((type) => {
+      if (queue[type].length !== 0 && searchStatus) {
+        const result = process(type, queue[type]);
+        if (result !== false) {
+          searchStatus = false;
+          contactNode = result;
+        }
+      }
+    });
+  }
+  return constructPath(contactNode);
+}
+
+function constructPath(node) {
+  const path = [node.id];
+  const probe = (type, currentNode, pathArray = path) => {
+    const nextNode = currentNode.visitors[type].reduce((final, current) => {
+      if (final.steps > current.steps) {
+        final = current;
+      }
+      return final;
+    }).node;
+    if (nextNode !== null) {
+      if (type === "base") {
+        pathArray.unshift(nextNode.id);
+      } else {
+        pathArray.push(nextNode.id);
+      }
+      probe(type, nextNode, pathArray);
+    }
+  };
+  probe("base", node);
+  probe("target", node);
+
+  return path;
+}
+
+function knightMoves(baseLoc, targetLoc, board) {}
 
 const board = generateBoard(8, 8);
-
-console.log(board);
